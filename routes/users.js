91d55307dab1b6c845750e30813e01798086d8ca
adjiemkyn <adjiemikayana@gmail.com>
2024-12-23 10:58:48 +0800

    user

--- routes/users.js
+++ routes/users.js
@@ -79,7 +79,7 @@ router.delete('/:id', (req, res) => {
             res.status(404).json({ error: 'User not found' });
             return;
         }
-        res.json({ message: 'User deleted successfully' });
+        res.json({ message: 'j' });
     });
 });
 
